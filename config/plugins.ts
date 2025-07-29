7b4781da0233b276cef66194b14fe283a567fc7b
minothegreat881 <minothegreat881@gmail.com>
2025-07-29 09:53:54 +0200

    Add Cloudinary upload provider configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,16 @@
-export default () => ({
+export default ({ env }) => ({
   upload: {
     config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
       sizeLimit: 10 * 1024 * 1024, // 10MB maximum
       breakpoints: {
         xlarge: 1920,
